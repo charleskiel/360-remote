d5b89d54fde6f84e0cb9a6d9df2e8369126cf358
Charles Kiel <charleskiel@yahoo.com>
2020-08-27 16:03:10 -0600

    adding video player

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -2,92 +2,124 @@ import React from "react";
 import Indicator from "./Indicator"
 import { Progress, Tabs,Button } from 'antd'
 import Icon from "@ant-design/icons/lib/components/Icon";
+import "./Controller.scss";
+import videojs from "video.js";
+//import "video.js/dist/video-js.css";
 
-import {MoreOutlined } from '@ant-design/icons';
+import { MoreOutlined } from '@ant-design/icons';
+
+import "https://vjs.zencdn.net/7.8.4/video-js.css"
+import "https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"
 
 const { TabPane } = Tabs;
 class Controller extends React.Component {
-//start here with your code for step one
-	state = {
-	selectedVideo: null
-	}
+	//start here with your code for step one
+	videoJsOptions = {
+		autoplay: true,
+		controls: true,
+		sources: [
+			{
+				src: "http://stream.360tv.net:8080/live/360/index.m3u8",
+				type: "application/x-mpegURL",
+			},
+		],
+	};
 
+	startVideo(video) {
+		videojs(video);
+    	}
+
+	
+	state = {
+		selectedVideo: null,
+	};
 
 	goback = () => {
-	this.setState({ selectedVideo: null })
-	}
-
+		this.setState({ selectedVideo: null });
+	};
 
 	showdetails = (Video) => {
-	this.setState({ showdetails: true, selectedVideo: Video })
-	}
-
-
-
+		this.setState({ showdetails: true, selectedVideo: Video });
+	};
 
 	render() {
-	return (
+		return (
+			<div>
+				{this.props.status.videos.length > 0 && (
+					<Tabs defaultActiveKey="1" className={"controller"}>
+						<TabPane tab="Playing" key="1">
+							<div style={{ float: "left", padding: "1em" }}>
+								<Progress
+									type="circle"
+									strokeColor={{
+										"0%": "#1000e9",
+										"100%": "#FF0000",
+									}}
+									status="active"
+									width={75}
+									strokeWidth={4}
+									percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)}
+								/>
+							</div>
+							{this.props.status.controllerTickStatus.contentType === "MusicVideo" && (
+								<h4>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${
+									this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title
+								}`}</h4>
+							)}
+							{/* <Progress showinfo={"false"} size="small" percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)} /> */}
 
+							<p className={"text-main text-semibold"}>{this.props.toHHMMSS(this.props.status.controllerTickStatus.time)}</p>
+							<p>
+								<Indicator indicator={this.props.status.controllerTickStatus.timestamp} type="square" />
+								<span className="label label-success">{this.props.status.controllerTickStatus.contentType}</span>
+							</p>
+							<table>
+								<tr>
+									<td>
+										<Indicator />{" "}
+									</td>
+								</tr>
+								<tr>
+									<Button ghost size="small">
+										<MoreOutlined />
+									</Button>
+								</tr>
+							</table>
 
-		<div >
-		{this.props.status.videos.length > 0 &&
+							<video ref={this.startVideo} width={180} height={320} className="video-js vjs-default-skin" controls>
+								<source src={this.props.source} type="application/x-mpegURL" />
+							</video>
+						</TabPane>
 
-		<Tabs defaultActiveKey="1"
-			className={"controller"}>
-			<TabPane tab="Playing" key="1">
-				<div style={{ float: "left", padding: "1em" }}>
-				<Progress
-					type="circle"
-					strokeColor={{
-					'0%': '#1000e9',
-					'100%': '#FF0000',
-					}}
-					status="active"
-					width={75}
-					strokeWidth={4}
-					percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)}
-						/>
-						
-				</div>
-				{this.props.status.controllerTickStatus.contentType === "MusicVideo" && <h4>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title}` }</h4>}
-				{/* <Progress showinfo={"false"} size="small" percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)} /> */}
-
-				<p className={"text-main text-semibold"}>{this.props.toHHMMSS(this.props.status.controllerTickStatus.time)}</p>
-				<p><Indicator indicator={this.props.status.controllerTickStatus.timestamp} type="square" /><span className="label label-success">{this.props.status.controllerTickStatus.contentType}</span></p>
-					<table>
-						<tr>
-							<td><Indicator/> </td>
-						</tr>
-						<tr>
-						<Button ghost size="small"><MoreOutlined /></Button>
-						</tr>
-					</table>	
-
-			</TabPane>
-
-
-
-			{this.props.status.controllerTickStatus.contentType === "MusicVideo" &&			
-				<TabPane tab="Details" key="2">
-					<h2>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title}` }</h2>
-						<small>
-							<p>{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist}</p>
-							<p>&quot;{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title}&quot; {this.props.status.videos[this.props.status.controllerTickStatus.contentId].AltTitle && ` (${this.props.status.videos[this.props.status.controllerTickStatus.contentId].AltTitle})`}</p>
-							<p>{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Album}</p>
-							<p>{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Label} ({this.props.status.videos[this.props.status.controllerTickStatus.contentId].Year})</p>
-							<p>Director: {this.props.status.videos[this.props.status.controllerTickStatus.contentId].Director}</p>
-						</small>
-				</TabPane>
-			}
-			<TabPane tab="Tab 3" key="3">
-				{this.props.status.controllerTickStatus.contentId}
-			</TabPane>
-		</Tabs>
-			}
-	</div>
-
-	);
-
-}}
+						{this.props.status.controllerTickStatus.contentType === "MusicVideo" && (
+							<TabPane tab="Details" key="2">
+								<h2>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${
+									this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title
+								}`}</h2>
+								<small>
+									<p>{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist}</p>
+									<p>
+										&quot;{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title}&quot;{" "}
+										{this.props.status.videos[this.props.status.controllerTickStatus.contentId].AltTitle &&
+											` (${this.props.status.videos[this.props.status.controllerTickStatus.contentId].AltTitle})`}
+									</p>
+									<p>{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Album}</p>
+									<p>
+										{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Label} (
+										{this.props.status.videos[this.props.status.controllerTickStatus.contentId].Year})
+									</p>
+									<p>Director: {this.props.status.videos[this.props.status.controllerTickStatus.contentId].Director}</p>
+								</small>
+							</TabPane>
+						)}
+						<TabPane tab="Tab 3" key="3">
+							{this.props.status.controllerTickStatus.contentId}
+						</TabPane>
+					</Tabs>
+				)}
+			</div>
+		);
+	}
+}
 
 export default Controller;
