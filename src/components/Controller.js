28b9f13c05b54928a6718a7aa23761e094996f08
Charles Kiel <charleskiel@yahoo.com>
2020-09-02 21:55:53 -0600

    dark mode

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -3,15 +3,24 @@ import Indicator from "./Indicator"
 import { Progress, Tabs,Button } from 'antd'
 import Icon from "@ant-design/icons/lib/components/Icon";
 import "./Controller.scss";
-//import videojs from "video.js";
-//import "video.js/dist/video-js.css";
-
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import ReactDOM from "react-dom";
 import { MoreOutlined } from '@ant-design/icons';
-
+import VideoPlayer from 'react-video-js-player';
 
     
 const { TabPane } = Tabs;
 class Controller extends React.Component {
+
+	constructor(props) {
+		super(props);
+		// create a ref to store the textInput DOM element
+		this.videoNode = React.createRef();
+		//this.focusTextInput = this.focusTextInput.bind(this);
+	   }
+
+
 	//start here with your code for step one
 	videoJsOptions = {
 		autoplay: true,
@@ -28,10 +37,13 @@ class Controller extends React.Component {
 		videojs(video);
     	}
 
+	    
 	componentDidMount = () => {
-		    this.player = videojs(this.videoNode, this.videoJsOptions, function onPlayerReady() {
-				console.log("Video.js Ready", this);
-			});
+
+		// console.log(this.videoNode)
+		//     this.player = videojs(this.videoNode, this.videoJsOptions, function onPlayerReady() {
+		// 		console.log("Video.js Ready", this);
+		// 	});
 	}
 
 
@@ -48,59 +60,74 @@ class Controller extends React.Component {
 	};
 
 	render() {
+		this.refs = React.createRef();
+
 		return (
 			<div>
 				{this.props.status.videos.length > 0 && (
 					<Tabs defaultActiveKey="1" className={"controller"}>
 						<TabPane tab="Playing" key="1">
-							<div style={{ float: "left", padding: "1em" }}>
-								<Progress
-									type="circle"
-									strokeColor={{
-										"0%": "#1000e9",
-										"100%": "#FF0000",
-									}}
-									status="active"
-									width={75}
-									strokeWidth={4}
-									percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)}
-								/>
-							</div>
-							{this.props.status.controllerTickStatus.contentType === "MusicVideo" && (
-								<h4>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${
-									this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title
-								}`}</h4>
-							)}
-							{/* <Progress showinfo={"false"} size="small" percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)} /> */}
-
-							<p className={"text-main text-semibold"}>{this.props.toHHMMSS(this.props.status.controllerTickStatus.time)}</p>
-							<p>
-								<Indicator indicator={this.props.status.controllerTickStatus.timestamp} type="square" />
-								<span className="label label-success">{this.props.status.controllerTickStatus.contentType}</span>
-							</p>
 							<table>
 								<tr>
+									<td style={{width:"50%"}}>
+
+										<div style={{ float: "left", padding: "1em" }}>
+											<Progress
+												type="circle"
+												strokeColor={{
+													"0%": "#1000e9",
+													"100%": "#FF0000",
+												}}
+												status="active"
+												width={75}
+												strokeWidth={4}
+												percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)}
+											/>
+										</div>
+										{this.props.status.controllerTickStatus.contentType === "MusicVideo" && (
+											<h4>{`${this.props.status.videos[this.props.status.controllerTickStatus.contentId].Artist} - ${
+												this.props.status.videos[this.props.status.controllerTickStatus.contentId].Title
+											}`}</h4>
+										)}
+										{/* <Progress showinfo={"false"} size="small" percent={Math.round((this.props.status.controllerTickStatus.frame / this.props.status.controllerTickStatus.frames) * 100)} /> */}
+
+										<p className={"text-main text-semibold"}>{this.props.toHHMMSS(this.props.status.controllerTickStatus.time)}</p>
+										<p>
+											<Indicator indicator={this.props.status.controllerTickStatus.timestamp} type="square" />
+											<span className="label label-success">{this.props.status.controllerTickStatus.contentType}</span>
+										</p>
+
+										<table>
+											<tr>
+												<td>
+													<Indicator />{" "}
+												</td>
+											</tr>
+											<tr>
+												<Button ghost size="small">
+													<MoreOutlined />
+												</Button>
+											</tr>
+										</table>
+
+									</td>
 									<td>
-										<Indicator />{" "}
+
+										<videojs
+											ref={this.videoNode}
+											id="my-video"
+											class="video-js"
+											controls
+											preload="auto"
+											width="640"
+											height="264"
+											poster="../images/Image1.png"
+											data-setup="{}"
+										></videojs>
 									</td>
 								</tr>
-								<tr>
-									<Button ghost size="small">
-										<MoreOutlined />
-									</Button>
-								</tr>
 							</table>
-							<video
-								ref={(node) => (this.videoNode = node)}
-								id="my-video"
-								class="video-js"
-								controls
-								preload="auto"
-								width="640"
-								height="264"
-								poster="../images/Image1.png"
-								data-setup="{}"
-							>
+
 								{/* 
 							<p class="vjs-no-js">
 								To view this video please enable JavaScript, and consider upgrading to a
@@ -109,7 +136,6 @@ class Controller extends React.Component {
 								>supports HTML5 video</a
 								>
 							</p> */}
-							</video>
 						</TabPane>
 
 						{this.props.status.controllerTickStatus.contentType === "MusicVideo" && (
@@ -133,7 +159,7 @@ class Controller extends React.Component {
 								</small>
 							</TabPane>
 						)}
-						<TabPane tab="Tab 3" key="3">
+						<TabPane tab="History" key="3">
 							{this.props.status.controllerTickStatus.contentId}
 						</TabPane>
 					</Tabs>
